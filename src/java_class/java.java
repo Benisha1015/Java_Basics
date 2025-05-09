54d1b607107552845f46f97bc9b08cc1b8f0864b
Benisha1015 <benisha1015@gmail.com>
2025-05-09 13:33:16 +0530

    Initial commit

---
+++ src/java_class/java.java
@@ -0,0 +1,7 @@
+package java_class;
+
+public class java {
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+    }
+}
---
+++ src/test_my_second_code.java
@@ -0,0 +1,6 @@
+public class test_my_second_code {
+
+    public static void main(String[] args) {
+        System.out.println("code test");
+    }
+}
